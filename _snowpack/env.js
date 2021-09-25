30f8603a82149eb75fbc83e95f5025e95efb8003
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-25 14:00:41 +0000

    deploy: 9a1b90efc15ab26f320aac54e0003b8edfc85a55

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "026bdac113c1e84c6d66e1163ce9ad96a56d39b8";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.6";
+export const SNOWPACK_PUBLIC_GIT_SHA = "9a1b90efc15ab26f320aac54e0003b8edfc85a55";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.7";
 export const SNOWPACK_PUBLIC_URL = "/solve-2nd-deg-equation";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- home.js
+++ home.js
@@ -2,5 +2,9 @@ import React from "./_snowpack/pkg/react.js";
 import {title} from "./config.js";
 import UI from "./ui/index.js";
 export default () => {
-  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, title), /* @__PURE__ */ React.createElement(UI, null));
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, title), /* @__PURE__ */ React.createElement("div", {
+    className: "row"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "col-md-6"
+  }, /* @__PURE__ */ React.createElement(UI, null))));
 };
