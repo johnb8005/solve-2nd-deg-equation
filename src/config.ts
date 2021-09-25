47ee50b8e7e15539924f31a0e695ba0e6b89a44e
John B <john@nexys.ch>
2021-09-25 12:47:24 +0200

    title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const version =
   import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
 export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
-export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "Boilerplate";
+export const title =
+  import.meta.env.SNOWPACK_PUBLIC_TITLE || "Solve 2nd Degree Equation";
 export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
 
 const ghUrl = "https://github.com/johnb8005/solve-2nd-deg-equation";
