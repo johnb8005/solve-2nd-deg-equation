388661e202cf1b07e1fa6d7af24ab59ef5717201
johnb8005 <johnb8005@users.noreply.github.com>
2021-10-14 11:32:56 +0000

    deploy: d9a1f65b8fef3de49859c97aa642948cfda9c711

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "860afd0a18853857257304ea343a589fca7a400f";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.8";
+export const SNOWPACK_PUBLIC_GIT_SHA = "d9a1f65b8fef3de49859c97aa642948cfda9c711";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.9";
 export const SNOWPACK_PUBLIC_URL = "/solve-2nd-deg-equation";
 export const MODE = "development";
 export const NODE_ENV = "development";
